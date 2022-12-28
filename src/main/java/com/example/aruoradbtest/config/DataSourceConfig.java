8a4d9ca16653e9512f7dbe91ecb51357d167c776
ujjal38 <kundu.vivasoft@bkash.com>
2022-12-28 12:37:11 +0600

    added dynamic data source for read write segregation

---
+++ src/main/java/com/example/aruoradbtest/config/DataSourceConfig.java
@@ -0,0 +1,41 @@
+package com.example.aruoradbtest.config;
+
+import com.example.aruoradbtest.config.datasource.DynamicDataSource;
+import com.example.aruoradbtest.enums.DataSourceType;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.writer")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.reader")
+    public DataSource salveDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "dynamicDataSource")
+    @Primary
+    public DynamicDataSource dataSource(DataSource masterDataSource, DataSource slaveDataSource){
+        Map<Object,Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.WRITER.name(),masterDataSource);
+        targetDataSources.put(DataSourceType.READER.name(),slaveDataSource);
+        return new DynamicDataSource(masterDataSource,targetDataSources);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/aruoradbtest/config/datasource/DynamicDataSource.java
@@ -0,0 +1,20 @@
+package com.example.aruoradbtest.config.datasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    public DynamicDataSource(DataSource defaultDataSource, Map<Object, Object> targetDataSources){
+        super.setDefaultTargetDataSource(defaultDataSource);
+        super.setTargetDataSources(targetDataSources);
+        super.afterPropertiesSet();
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DynamicDataSourceContextHolder.getDataSourceType();
+    }
+}
